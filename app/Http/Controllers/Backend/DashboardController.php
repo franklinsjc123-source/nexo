69d0d8bbf44b214875c210c1926efd1e4b17ee14
franklin <franklin@alphasoftz.in>
2026-02-13 18:26:43 +0530

    changes

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Shop;
+use App\Models\User;
+use App\Models\Order;
+use App\Models\DirectOrder;
+use App\Models\DeliveryPerson;
 
 use Illuminate\Http\Request;
 
@@ -11,15 +16,15 @@ class DashboardController extends Controller
 {
     public function dashboard()
     {
+        
+        $shop_count             = count(Shop::where('status',1)->get());
+        $customer_count         = count(User::where('auth_level',3)->where('status',1)->get());
+        $order_count            = count(Order::get());
+        $direct_order_count     = count(DirectOrder::get());
+        $delivert_person_count  = count(DeliveryPerson::where('status',1)->get());
 
-
-
-        return view('backend.dashboard');
+        return view('backend.dashboard',compact('shop_count','customer_count','order_count','delivert_person_count','direct_order_count','delivert_person_count'));
     }
 
-    public function blank()
-    {
-        return view('backend.blank');
-    }
 
 }
