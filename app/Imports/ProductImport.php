07cd803aa790428a3b159fafc8b7a028830b9be9
franklin <franklin@alphasoftz.in>
2026-02-20 22:18:53 +0530

    chnages

--- app/Imports/ProductImport.php
+++ app/Imports/ProductImport.php
@@ -80,48 +80,4 @@ class ProductImport implements ToCollection, WithHeadingRow
     }
 
 
-
-    // public function collection(Collection $rows)
-    // {
-
-
-    //     foreach ($rows as $row) {
-
-    //         if (
-    //             empty($row['product_name']) &&
-    //             empty($row['category']) &&
-    //             empty($row['shop'])
-    //         ) {
-    //             continue;
-    //         }
-
-    //         if (empty($row['product_name'])) {
-    //             $this->skipped++;
-    //             continue;
-    //         }
-
-
-
-    //         $category = Category::where('category_name', $row['category'])->first();
-    //         $shop = Shop::where('shop_name', $row['shop'])->first();
-    //         $unit = Unit::where('unit_name', $row['unit'])->first();
-
-    //         if (!$category || !$shop) {
-    //             $this->skipped++;
-    //             continue;
-    //         }
-
-    //         Product::create([
-    //             'category'            => $category->id,
-    //             'shop'                => $shop->id,
-    //             'qty'                 => $row['qty'],
-    //             'unit'                => isset($unit->id) ?  $unit->id : '',
-    //             'product_name'        => $row['product_name'],
-    //             'original_price'      => $row['original_price'],
-    //             'discount_price'      => $row['discount_price'],
-    //             'product_description' => $row['product_description'],
-    //         ]);
-    //         $this->imported++;
-    //     }
-    // }
 }
