32bc4987021ae1fc6d044fd06155bbb2f5171f17
franklin <franklin@alphasoftz.in>
2026-02-21 18:05:09 +0530

    changes

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -21,7 +21,7 @@ class HomeController extends Controller
 
         if ($pincode) {
 
-            $checkPincodeExistence = PinCode::where('pincode', $pincode)->exists();
+            $checkPincodeExistence = PinCode::where('pincode', $pincode)->where('status', 1)->exists();
 
             $category   = Category::where('status', 1)->get();
             $shops      = Shop::where('status', 1)->inRandomOrder()->limit(8)->get();
