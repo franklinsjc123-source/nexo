7b305ca0fad460299e03134e96c06f3a84c6dfdd
franklin <franklin@alphasoftz.in>
2026-02-23 23:16:03 +0530

    changes

--- resources/views/backend/invoice/generate_abstract.blade.php
+++ resources/views/backend/invoice/generate_abstract.blade.php
@@ -20,24 +20,25 @@
 </head>
 <body>
 
-     <table width="100%" style=" border-radius: 50px; margin-top:-20px; border:none !important; border-collapse:collapse;">
+     <table width="100%" style=" margin-top:-20px; border:none !important; border-collapse:collapse;">
         <tr style="border:none !important;">
 
             <td width="10%" style="border:none;">
-                <img src="{{ $company->logo  }}" style="height :80px; width:80px">
+                <img src="{{ $company->logo  }}" style="  border-radius: 50px; height :80px; width:80px">
             </td>
 
-            <td width="60%" style="border:none; text-align:center;">
+            <td width="55%" style="border:none; text-align:center;">
                 <h2 style="margin:0;"> NEXOCART</h2>
                 <p style="margin:5px 0;">
-                <b>{{ $company->company_address }} , <br>{{  $company->state }}  - {{  $company->pincode }} .</b> <br>
-                <b>  Email :  {{ $company->email }} </b> </p>
+                <b>{{ $company->company_address }} , <br>{{  $company->state }}  .</b> <br>
+                <b>  Mobile :  {{ $company->phone }} </b> </p>
 
 
             </td>
-            <td width="30%" style="border:none !important;" class="right" colspan="2">
+            <td width="35%" style="border:none !important;" class="right" colspan="2">
                 <b>GST No: {{ $company->gst_no }}</b><br>
-                <b>  Mobile :  {{ $company->phone }} </b> </p>
+                <b>  Email :  {{ $company->email }} </b> </p>
+
 
 
             </td>
