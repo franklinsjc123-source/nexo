3f18e200eff1a299c5ac946ca21864c465e3a876
franklin <franklin@alphasoftz.in>
2026-02-27 21:03:29 +0530

    changes

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -1,6 +1,16 @@
 @extends('backend.app_template')
 @section('title','Dashboard')
 @section('content')
+
+<style>
+.chart-box {
+    position: relative;
+    height: 350px;   /* same height */
+    width: 100%;
+}
+</style>
+
+
 <main class="app-wrapper">
     <div class="container-fluid">
 
@@ -156,21 +166,31 @@
 
         </div>
 
+        <?php  if (Auth::user()->auth_level != 4) { ?>
+            <div class="card mt-4">
+                <div class="card-body text-center">
+                    <div class="row">
+                        {{-- <div class="col-md-1"></div> --}}
 
-        <div class="card mt-4">
-            <div class="card-body text-center">
-                <div class="row">
-                    <div class="col-md-6">
-                        <h5>Shops Based on Category</h5>
-                        <canvas id="categoryPieChart"></canvas>
-                    </div>
-                    <div class="col-md-6">
-                        <h5>Order and Direct Orders</h5>
-                        {{-- <canvas id="categoryPieChart"></canvas> --}}
+                        <div class="col-md-6">
+                            <h5  style="margin-left:-90px" >Shops Based on Category</h5>
+                            <div  style="cursor:pointer" class="chart-box">
+                                <canvas id="categoryPieChart"></canvas>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <h5 style="margin-left:-90px" >Order and Direct Orders</h5>
+                            <div  style="cursor:pointer" class="chart-box">
+                                <canvas id="orderPieChart"></canvas>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
-        </div>
+        <?php  } ?>
+
 
 
 
@@ -180,48 +200,86 @@
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script>
-    var ctx = document.getElementById('categoryPieChart').getContext('2d');
+  var ctx = document.getElementById('categoryPieChart').getContext('2d');
 
-    new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: {!! json_encode($categoryLabels) !!},
-            datasets: [{
-                data: {!! json_encode($shopCounts) !!}, // pie size based on shops
-                backgroundColor: [
-                    '#FF6384',
-                    '#36A2EB',
-                    '#FFCE56',
-                    '#4BC0C0',
-                    '#9966FF',
-                    '#FF9F40'
-                ]
-            }]
-        },
-        options: {
-            responsive: true,
-            plugins: {
-                tooltip: {
-                    callbacks: {
-                        label: function(context) {
+new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: {!! json_encode($categoryLabels) !!},
+        datasets: [{
+            data: {!! json_encode($shopCounts) !!},
+            backgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+                '#FFCE56',
+                '#4BC0C0',
+                '#9966FF',
+                '#FF9F40'
+            ]
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,   // 👈 important
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: function(context) {
 
-                            var shopCounts = {!! json_encode($shopCounts) !!};
-                            var productCounts = {!! json_encode($productCounts) !!};
+                        var shopCounts = {!! json_encode($shopCounts) !!};
+                        var productCounts = {!! json_encode($productCounts) !!};
 
-                            var index = context.dataIndex;
+                        var index = context.dataIndex;
 
-                            return [
-                                "Shops: " + shopCounts[index],
-                                "Products: " + productCounts[index]
-                            ];
-                        }
+                        return [
+                            "Shops: " + shopCounts[index],
+                            "Products: " + productCounts[index]
+                        ];
                     }
-                },
-                legend: {
-                    position: 'bottom'
                 }
+            },
+            legend: {
+                position: 'right'   // 👈 vertical legend
             }
         }
-    });
+    }
+});
+
+   var orderCtx = document.getElementById('orderPieChart').getContext('2d');
+
+new Chart(orderCtx, {
+    type: 'pie',
+    data: {
+        labels: ['Orders', 'Direct Orders'],
+        datasets: [{
+            data: [
+                {{ $order_count }},
+                {{ $direct_order_count }}
+            ],
+            backgroundColor: [
+                '#36A2EB',
+                '#FF6384'
+            ]
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: function(context) {
+                        return context.label + ": " + context.raw;
+                    }
+                }
+            },
+            legend: {
+                position: 'right'  // 👈 Vertical legend
+            }
+        }
+    }
+});
+
+
 </script>
 @endsection
