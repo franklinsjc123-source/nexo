5ead10d9ed24612978c40807b67364ed64f9a5c7
franklin <franklin@alphasoftz.in>
2026-03-04 21:17:27 +0530

    changes

--- resources/views/backend/shop/list.blade.php
+++ resources/views/backend/shop/list.blade.php
@@ -28,6 +28,7 @@
                         <th>S.No</th>
                         <th>Category</th>
                         <th>Shop</th>
+                        <th>Ratings</th>
                         <th>Start Time</th>
                         <th>End Time</th>
                         <th>Shop Image</th>
@@ -44,6 +45,7 @@
                                 <td><?php echo $i + 1 ?></td>
                                 <td>{{ $row->category_names }}</td>
                                 <td><?= $row->shop_name ?></td>
+                                <td><?= $row->rating ?></td>
                                 <td><?= date('h:i A', strtotime($row->start_time)) ?></td>
                                 <td><?= date('h:i A', strtotime($row->end_time)) ?></td>
                                 <td> <img class="mt-2" src="<?= $row->file_path ?>" alt="image description" width="50" height="50"></td>
@@ -55,7 +57,7 @@
                                     @if(auth()->user()->hasPermission('Shop-Edit'))
                                         <a data-toggle="tooltip" data-placement="top" title="Edit" href="<?php echo route('addShop',[$row->id]) ?>" class="btn btn-sm btn-warning"><i class="bi bi-pencil-fill"></i></a>
                                     @endif
-                                    
+
                                     @if(auth()->user()->hasPermission('Shop-Delete'))
                                         <a data-toggle="tooltip" data-placement="top" title="Delete" data-original-title="Delete" href="javascript:void(0)" onclick="commonDelete('<?php echo $row->id ?>','Shop')" class="btn btn-sm btn-danger"><i class="bi bi-trash-fill"></i></a>
                                     @endif
