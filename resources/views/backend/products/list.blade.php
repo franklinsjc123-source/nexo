9f1982a7a37a2ec8e886eb2e8e40fe25ce01c02a
franklin <franklin@alphasoftz.in>
2026-03-11 21:46:34 +0530

    changes

--- resources/views/backend/products/list.blade.php
+++ resources/views/backend/products/list.blade.php
@@ -105,7 +105,7 @@
                                 <td>
                                     <?php if($row->product_image ) { ?>
                                     <img class="mt-2"
-                                        src="<?= !empty($row->product_image) ? $row->product_image : asset('backend_assets/images/no_Image.png') ?>" alt="image" width="50" height="50">                                <?php }  ?>
+                                        src="<?= !empty($row->product_image) ? $row->product_image : asset('backend_assets/images/no_image.png') ?>" alt="image" width="50" height="50">                                <?php }  ?>
 
                                 </td>
                                 <td><a data-placement="top" title="Status" data-original-title="Status" href="javascript:void(0)" onclick="changeStatus('<?php echo $row->id ?>','<?php echo ($row->status == 1) ? 0 : 1 ?>','Product')" class="badge bg-pill bg-<?php echo ($row->status == 1) ? 'success' : 'danger' ?>">
