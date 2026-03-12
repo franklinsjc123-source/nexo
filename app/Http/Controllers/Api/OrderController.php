83f6a1ab4fb2225aec9e3f23365f30aa90feefae
franklin <franklin@alphasoftz.in>
2026-03-12 12:45:41 +0530

    changes

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -431,8 +431,8 @@ class OrderController extends Controller
 
                 /* ---------------- CLEAR CART ---------------- */
 
-                // CartItems::where('cart_id', $cart->id)->delete();
-                // $cart->delete();
+                CartItems::where('cart_id', $cart->id)->delete();
+                $cart->delete();
 
                 DB::commit();
 
