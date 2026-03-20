4ee3236127bc748d0a44f93a0a69577679863e92
franklin <franklin@alphasoftz.in>
2026-03-20 22:49:35 +0530

    changes

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -768,6 +768,7 @@ class OrderController extends Controller
                     Invoice::create([
                         'order_id'     => $order->id,
                         'shop_id'      => $shop_id,
+                        'final_shop_total'      => $final_shop_total,
                         'invoice_path' => URL::to('/') . '/uploads/shop_order_invoice/' . $shopInvoiceName
                     ]);
                 }
