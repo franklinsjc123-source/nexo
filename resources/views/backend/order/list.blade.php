1c634970abf3f0090621fa6ec942b6087fb36a11
franklin <franklin@alphasoftz.in>
2026-04-02 20:25:03 +0530

    changes

--- resources/views/backend/order/list.blade.php
+++ resources/views/backend/order/list.blade.php
@@ -70,7 +70,11 @@
 
                                 <td>
                                     <?php
-                                        if ($row->order_status == 1) {
+
+                                        if ($row->order_status == 4 || $row->is_dispatched == 1 ) {
+                                            $class = "secondary";
+                                            $text  = "Dispatched";
+                                        } else if ($row->order_status == 1) {
                                             $class = "warning";
                                             $text  = "New Order";
                                         } elseif ($row->order_status == 2) {
@@ -79,9 +83,6 @@
                                         } elseif ($row->order_status == 3) {
                                             $class = "danger";
                                             $text  = "Cancelled";
-                                        }  elseif ($row->order_status == 4 || $row->is_dispatched == 1 ) {
-                                            $class = "secondary";
-                                            $text  = "Dispatched";
                                         } else {
                                             $class = "secondary";
                                             $text  = "Unknown";
