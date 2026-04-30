c4a3ac54c2e1ba80b9706b45cbbdda07afa2dce8
franklin <franklin@alphasoftz.in>
2026-04-30 19:55:13 +0530

    changes

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -726,18 +726,20 @@ class OrderController extends Controller
     {
         $api = new Api(env('RAZORPAY_KEY'), env('RAZORPAY_SECRET'));
 
+
         try {
             $api->utility->verifyPaymentSignature([
-                // 'razorpay_order_id' => $request->razorpay_order_id,
+                'razorpay_order_id'   => $request->razorpay_order_id,
                 'razorpay_payment_id' => $request->razorpay_payment_id,
-                // 'razorpay_signature' => $request->razorpay_signature
+                'razorpay_signature'  => $request->razorpay_signature
             ]);
         } catch (\Exception $e) {
             return response()->json([
                 'status' => false,
-                'message' => 'Payment verification failed'
+                'message' => 'Payment verification failed: ' . $e->getMessage()
             ]);
         }
+
 
         $user_id        = $request->user_id;
         $delivery_id    = $request->delivery_id;
